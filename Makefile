4f539b410aebfebb7f86f0ae463ffa0fa27f4296
walid <wzakkabi@gmail.com>
2023-11-19 02:10:43 +0100

    draw first map in 2D

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = cc
-FLAGS = #-Werror -Wextra -Wall
+FLAGS = -fsanitize=address -g #-Werror -Wextra -Wall 
 SRCS = parsing.c map.c cub3d.c utils.c
 OBJS = $(SRCS:.c=.o)
 INCLUDES = -I./lib/MLX42/include/MLX42
--- utils.c
+++ utils.c
@@ -18,45 +18,46 @@ void	error_mlx(void)
 	exit(EXIT_FAILURE);
 }
 
-void	put_px(t_map *map, mlx_image_t *img, int x, int y, int how_many)
+void	put_px(t_map *map, mlx_image_t *img, int x, int y, uint32_t color)
 {
 	int tmp = 0;
 	int tx, ty;
 	tx = x;
 	ty = y;
-	while(tmp < how_many)
+	while(ty < y + 31)
 	{
-		while(ty < y + 32)
+		tx = x;
+		while(tx < x + 31)
 		{
-			tx = x;
-			while(tx < x + 32)
-			{
-				mlx_put_pixel(img, ty, tx, 0xFF0000FF);
-				tx++;
-			}
-			ty++;
+			mlx_put_pixel(img, tx, ty, color);
+			tx++;
 		}
+		ty++;
 	}
 }
+
 void	drow_map(t_map *map, mlx_image_t *img)
 {
 	int x;
 	int y;
+	int size;
+	uint32_t color;
 
 	x = 0;
 	y = 0;
 	while (map->map_s[y])
 	{
-		while(map->map_s[y][x])
+		x = -1;
+		size = 0;
+		while(map->map_s[y][++x])
 		{
+			color = 0xFF0000FF;
 			if(map->map_s[y][x] == '\t')
-				 put_px(map, img, x, y, 4);
-			if(map->map_s[y][x] == ' ')
-				put_px(map, img, x, y, 1);
-			if(map->map_s[y][x] == '0')
-				 put_px(map, img, x, y, 4);
-		
+				size = size +  (32 * 3);
+			if(map->map_s[y][x] == '1')
+				put_px(map, img, (x * 32) + size, y * 32, color);
 		}
+		y++;
 	}
 }
 
@@ -66,10 +67,10 @@ mlx_t	*init_mlx(t_map *map)
 	mlx_image_t *img;
 
 	printf("w = %d, h = %d\n", map->width_map, map->height_map);
-	mlx = mlx_init(map->width_map * 32, map->height_map * 32,"cub3d", true);
-	img = mlx_new_image(mlx, map->width_map * 32, map->height_map * 32);
+	mlx = mlx_init(map->width_map * 40, map->height_map * 40,"cub3d", true);
+	img = mlx_new_image(mlx, map->width_map * 40, map->height_map * 40);
 	if(!img || mlx_image_to_window(mlx, img, 0, 0) < 0)
 		error_mlx();
-	put_pix(map, img);
+	drow_map(map, img);
 	return (mlx);
 }
